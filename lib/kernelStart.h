91c17dba7b6800cd340326cdb56f95bf6086b6a6
ws23 <linyutsu23@gmail.com>
2015-03-25 20:36:21 +0800

    remove some lib

--- lib/kernelStart.h
+++
@@ -1,6 +0,0 @@
-#ifndef __KERNELSTART_H__
-#define __KERNELSTART_H__
-
-void vKernel_Initial(void);
-
-#endif
--- lib/timerSetup.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TIMERSETUP_H__
-#define __TIMERSETUP_H__
-
-void vOsTimer_Hardware_Setup(void);
-
-#endif
